09b8af9e92b594bb51ab5425aa6f0ff5be045a90
Kyle Hsu <Kyle Hsu>
2016-10-22 16:49:10 -0400

    for jimmy

--- js/scripts.js
+++ js/scripts.js
@@ -8,22 +8,42 @@
       scrollwheel: true,
       zoom: 8
     });
+    markers = [];
+    infowindows = [];
+    titles = ['isabelle', 'jimmy'];
+    lats = [52.35, 52.36];
+    lngs = [-1.17, -1.18];
+    contentStrings = ['hi im isabelle', 'hi im jimmy'];
 
-    var markers = [];
-    markers[0] = new google.maps.Marker({
-      position: {lat: 52.35, lng: -1.17},
-      map: map,
-      title: 'first marker'
-    });
+    for (i = 0; i < 2; i++) {
+      markers[i] = new google.maps.Marker({
+        position: {lat: lats[i], lng: lngs[i]},
+        map: map,
+        title: titles[i]
+      });
+      infowindows[i] = new google.maps.InfoWindow({
+        content: contentStrings[i]
+      });
+      markers[i].addListener('click', function() {
+        infowindows[i].open(map, markers[i])
+      });
+    }
 
-    var contentString = 'hello world';
-    var infowindow = new google.maps.InfoWindow({
-      content: contentString
-    })
+    // var marker = new google.maps.Marker({
+    //   position: {lat: 52.35, lng: -1.17},
+    //   map: map,
+    //   title: 'first marker'
+    // });
+    //
+    // var contentString = 'hello world';
+    // var infowindow = new google.maps.InfoWindow({
+    //   content: contentString
+    // })
+    //
+    // marker.addListener('click', function() {
+    //   infowindow.open(map, marker)
+    // });
 
-    marker.addListener('click', function() {
-      infowindow.open(map, marker)
-    });
   }
 
 
