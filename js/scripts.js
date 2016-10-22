f376c8589702b039f3f3f350041bc5403c80fc18
Kyle Hsu <Kyle Hsu>
2016-10-22 16:19:34 -0400

    lol

--- js/scripts.js
+++ js/scripts.js
@@ -4,16 +4,27 @@
   function initMap() {
     // Create a map object and specify the DOM element for display.
     var map = new google.maps.Map(document.getElementById('map-canvas'), {
-      center: {lat: -34.397, lng: 150.644},
+      center: {lat: 52.35, lng: -1.17},
       scrollwheel: true,
       zoom: 8
     });
+
+    var markers = [];
+    markers[0] = new google.maps.Marker({
+      position: {lat: 52.35, lng: -1.17},
+      map: map
+    });
+
+    var contentString = 'hello world';
+    var infowindow = new google.maps.InfoWindow({
+      content: contentString;
+    })
   }
-  
+
 
 /*function initialize() {
 
-  
+
   var latlng = new google.maps.LatLng(52.3731, 4.8922);
 
   var mapOptions = {
@@ -21,13 +32,13 @@
     scrollWheel: false,
     zoom: 13
   };
-  
+
   var marker = new google.maps.Marker({
     position: latlng,
     url: '/',
     animation: google.maps.Animation.DROP
   });
-  
+
   var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
   marker.setMap(map);
 
